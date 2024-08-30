cfc258fd2be45e15326a087cf0a3941a74dd2be2
javahuhu <cgelera3@gmail.com>
2024-08-30 08:16:42 +0800

    first commit

---
+++ myairline/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header"
+import Footer from "./assets/Footer";
+
+function App() {
+ return(
+  <>
+  <Header />
+  <Footer / >
+    </>
+ );
+}
+
+export default App
---
+++ myairline/src/Header.jsx
@@ -0,0 +1,16 @@
+function Header () {
+     
+    return( 
+        <header>
+            <h1>React first try</h1>
+            <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About us</a></li>
+            <li><a href="#">Service</a></li>
+            <li><a href="#">Settings</a></li>
+            </ul>
+        </header>
+    );
+}
+
+export default Header
